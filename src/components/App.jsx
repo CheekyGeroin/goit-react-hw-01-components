094c10262cb593cd7f4bfecafa98e3f957125d90
Danylo Altukhov <dan.97198@gmail.com>
2022-09-08 21:30:31 +0300

    make first task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,11 @@
+import user from './Profile/user'
+import data from './Statistics/data'
+import { Profile } from './Profile/Profile'
+import {Statistics} from './Statistics/Statistics'
+
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+  return (<div>
+    <Profile username={user.username} tag={user.tag} location={user.location} stats={user.stats} avatar={user.avatar} />
+    <Statistics title={data.title} stats={data.stats} />
+  </div>)
 };
